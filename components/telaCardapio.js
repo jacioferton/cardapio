6b73425931ace031a85eb7307ab57ab70477e89f
End-Star01 <129421702+jacioferton@users.noreply.github.com>
2023-05-09 10:11:46 -0300

    implementando o cardapio

--- components/telaCardapio.js
+++ components/telaCardapio.js
@@ -1,8 +1,12 @@
-import { Text, View } from 'react-native' 
+import { Pressable, Text, View } from 'react-native' 
 
 function TelaCardapio({navigation}) {
     return (
-        <View><Text>EM BREVE!</Text></View>
+        <View>
+            <Pressable>
+                <Text onPress ={ () => navigation.navigate('Login')}>EM BREVE!</Text>
+            </Pressable>
+        </View>
     );
 };
 
