6099ff0f0cd3dcc1188e45e6b5fcce5420178956
End-Star01 <129421702+jacioferton@users.noreply.github.com>
2023-05-09 13:02:51 -0300

    qa

--- App.js
+++ App.js
@@ -6,16 +6,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TelaLogin from './components/telaLogin';
 import TelaCadastro from './components/telaCadastro';
 import TelaCardapio from './components/telaCardapio';
+import TelaAtividade from './components/telaAtividade.js';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Atividade">
         <Stack.Screen name="Login" component={TelaLogin}/>
         <Stack.Screen name="Cadastro" component={TelaCadastro}/>
         <Stack.Screen name="Cardapio" component={TelaCardapio}/>
+        <Stack.Screen name="Atividade" component={TelaAtividade}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
